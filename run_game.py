e746ed8a43462a34026508d3e034d628745d6e16
Benedikt Witteler <benedikt.witteler@tum.de>
2022-04-12 11:04:01 +0200

    Change existing QAgent for SpaceInvaders game

--- run_game.py
+++ run_game.py
@@ -3,18 +3,30 @@ from game.SpaceInvaders import SpaceInvaders
 from controller.keyboard import KeyboardController
 from controller.random_agent import RandomAgent
 
+from epsilon_profile import EpsilonProfile
+from agent.qagent import QAgent
+
 
 def main():
 
+    n_episodes = 1000  # *************** 200 3000
+    max_steps = 500    # *************** 5060
+    gamma = 1.
+    alpha = 0.2
+    eps_profile = EpsilonProfile(1.0 , 0.1) # *** 1.0, 0.1
+
     game = SpaceInvaders(display=True)
-    controller = KeyboardController()
+    # controller = KeyboardController()
     # controller = RandomAgent(game.na)
+    controller = QAgent(game, eps_profile, gamma, alpha)
+    controller.learn(game, n_episodes, max_steps)
+
 
     state = game.reset()
     while True:
         action = controller.select_action(state)
         state, reward, is_done = game.step(action)
-        print("position of X"+str(game.get_state()))
+        # print("position of X"+str(game.get_state()))
         # print("position of Y" + str(game.get_player_Y())
         # print("position of X_invader" + str(game.get_invaders_X())
         # print("position of Y_invader" + str(game.get_invaders_Y())
