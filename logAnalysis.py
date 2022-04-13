b602a7e4881d36eabf4285bf701236031bf0fb18
Benedikt Witteler <benedikt.witteler@tum.de>
2022-04-13 10:34:28 +0200

    Copy visualization files into workspace

---
+++ logAnalysis.py
@@ -0,0 +1,15 @@
+import os
+import plotly.express as px
+import pandas as pd
+
+
+class logAnalysis:
+
+	def __init__(self, file : str):
+		self.file = file
+		self.values = []
+
+	def printCurves(self):
+		df = pd.read_csv(os.path.abspath("partie_3/visualisation/logQ.csv"))
+		fig = px.scatter(x=df["episode"], y=df["value"])
+		fig.show()
