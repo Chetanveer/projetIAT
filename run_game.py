4b59bec18c2177fb61b2266643f763a8666ed9c6
Benedikt Witteler <benedikt.witteler@tum.de>
2022-04-16 17:09:14 +0200

    Launch Trainings

--- run_game.py
+++ run_game.py
@@ -35,110 +35,47 @@ def main():
 
     #     # sleep(0.0001)
 
-    # # -------------------------------------------------------------------------
-    # for gamma in [0.95, 0.5, 0.1]:
-    #     for max_steps in [300, 400, 500, 600, 700, 800]:
-    #         n_episodes = 10000
-    #         # max_steps = 3000
-    #         # gamma = 0.95
-    #         alpha = 1
-    #         eps_profile = EpsilonProfile(1.0, 0.1)
-    #         fileName = "Q_{}_E{}_S{}_G{}_I{}_F{}".format("SXY", n_episodes, max_steps, gamma,
-    #                                                 eps_profile.initial,
-    #                                                 eps_profile.final)
-    #         controller = QAgent(game, eps_profile, gamma, alpha, fileName)
+    # -------------------------------------------------------------------------
+    for eps_i in [0.95, 0.5, 0.1]:
+        for eps_f in [0.1, 0.0, 0.01]:
+            for max_steps in [1000]:
+                n_episodes = 500000
+                # max_steps = 3000
+                gamma = 0.95
+                alpha = 1
+                eps_profile = EpsilonProfile(eps_i, eps_f)
+                fileName = "Q_{}_E{}_S{}_G{}_I{}_F{}".format("SXY", n_episodes, max_steps, gamma,
+                                                        eps_profile.initial,
+                                                        eps_profile.final)
+                controller = QAgent(game, eps_profile, gamma, alpha, fileName)
 
-    #         startTime = time()
-    #         controller.learn(game, n_episodes, max_steps)
-    #         endTime = time()
-    #         controller.saveQToFile(os.path.join("LearnedQ", fileName))
+                startTime = time()
+                controller.learn(game, n_episodes, max_steps)
+                endTime = time()
+                controller.saveQToFile(os.path.join("LearnedQ", fileName))
 
-    #         print()
-    #         print(
-    #             "############################################################################"
-    #         )
-    #         print("FINISHED LEARNING")
-    #         print("    n_episodes: ", n_episodes)
-    #         print("    max_steps: ", max_steps)
-    #         print("    gamma: ", gamma)
-    #         print("    eps_profile (initial, final, dec_episode, dec_step): ",
-    #             eps_profile.initial, eps_profile.final, eps_profile.dec_episode,
-    #             eps_profile.dec_step)
-    #         print("    time learning: ", endTime - startTime)
-    #         print(
-    #             "############################################################################"
-    #         )
+                print()
+                print(
+                    "############################################################################"
+                )
+                print("FINISHED LEARNING")
+                print("    n_episodes: ", n_episodes)
+                print("    max_steps: ", max_steps)
+                print("    gamma: ", gamma)
+                print("    eps_profile (initial, final, dec_episode, dec_step): ",
+                    eps_profile.initial, eps_profile.final, eps_profile.dec_episode,
+                    eps_profile.dec_step)
+                print("    time learning: ", endTime - startTime)
+                print(
+                    "############################################################################"
+                )
 
     # -------------------------------------------------------------------------
-    n_episodes = 2000
-    max_steps = 700
-    gamma = 0.95
-    alpha = 1
-    eps_profile = EpsilonProfile(0.9, 0.1)
-    fileName = "Q_{}_E{}_S{}_G{}_I{}_F{}".format("SXY", n_episodes, max_steps, gamma,
-                                              eps_profile.initial,
-                                              eps_profile.final)
-    controller = QAgent(game, eps_profile, gamma, alpha, fileName)
-
-    startTime = time()
-    controller.learn(game, n_episodes, max_steps)
-    endTime = time()
-    controller.saveQToFile(os.path.join("LearnedQ", fileName))
-
-    print()
-    print(
-        "############################################################################"
-    )
-    print("FINISHED LEARNING")
-    print("    n_episodes: ", n_episodes)
-    print("    max_steps: ", max_steps)
-    print("    gamma: ", gamma)
-    print("    eps_profile (initial, final, dec_episode, dec_step): ",
-          eps_profile.initial, eps_profile.final, eps_profile.dec_episode,
-          eps_profile.dec_step)
-    print("    time learning: ", endTime - startTime)
-    print(
-        "############################################################################"
-    )
-
-    # # -------------------------------------------------------------------------
-    # n_episodes = 10000
-    # max_steps = 5000
-    # gamma = 0.999
-    # alpha = 1
-    # eps_profile = EpsilonProfile(1.0, 0.1)
-    # fileName = "Q_{}_E{}_S{}_G{}_I{}_F{}".format("SXY", n_episodes, max_steps, gamma,
-    #                                           eps_profile.initial,
-    #                                           eps_profile.final)
-    # controller = QAgent(game, eps_profile, gamma, alpha, fileName)
-
-    # startTime = time()
-    # controller.learn(game, n_episodes, max_steps)
-    # endTime = time()
-    # controller.saveQToFile(os.path.join("LearnedQ", fileName))
-
-    # print()
-    # print(
-    #     "############################################################################"
-    # )
-    # print("FINISHED LEARNING")
-    # print("    n_episodes: ", n_episodes)
-    # print("    max_steps: ", max_steps)
-    # print("    gamma: ", gamma)
-    # print("    eps_profile (initial, final, dec_episode, dec_step): ",
-    #       eps_profile.initial, eps_profile.final, eps_profile.dec_episode,
-    #       eps_profile.dec_step)
-    # print("    time learning: ", endTime - startTime)
-    # print(
-    #     "############################################################################"
-    # )
-
-    # # -------------------------------------------------------------------------
-    # n_episodes = 20000
-    # max_steps = 5000
+    # n_episodes = 2000
+    # max_steps = 700
     # gamma = 0.95
     # alpha = 1
-    # eps_profile = EpsilonProfile(1.0, 0.5)
+    # eps_profile = EpsilonProfile(0.9, 0.1)
     # fileName = "Q_{}_E{}_S{}_G{}_I{}_F{}".format("SXY", n_episodes, max_steps, gamma,
     #                                           eps_profile.initial,
     #                                           eps_profile.final)
@@ -164,9 +101,6 @@ def main():
     # print(
     #     "############################################################################"
     # )
-
-    # -------------------------------------------------------------------------
-
 
 if __name__ == '__main__':
     main()
