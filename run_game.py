c1ce87c432e46ed964ba5ae5f12810f03dd50224
Benedikt Witteler <benedikt.witteler@tum.de>
2022-04-21 13:54:41 +0200

    Find out after how many episodes agent is best

--- run_game.py
+++ run_game.py
@@ -7,40 +7,43 @@ from controller.random_agent import RandomAgent
 from epsilon_profile import EpsilonProfile
 from agent.qagent import QAgent
 
+import logging
+
 
 def main():
     game = SpaceInvaders(display=False)
 
-    for eps_i in [0.7]: #0.6
-        for eps_f in [0.05]:
-            n_episodes = 200000
-            max_steps = 500
-            gamma = 0.95
-            alpha = 1
-            eps_profile = EpsilonProfile(eps_i, eps_f)
-            fileName = "Q_{}_E{}_S{}_G{}_I{}_F{}".format("SXY", n_episodes, max_steps,
-                                                        gamma, eps_profile.initial,
-                                                        eps_profile.final)
-            controller = QAgent(game, eps_profile, gamma, alpha, fileName)
+    # for eps_i in [0.7]: #0.6
+    #     for eps_f in [0.05]:
+    #         n_episodes = 200000
+    #         max_steps = 500
+    #         gamma = 0.95
+    #         alpha = 1
+    #         eps_profile = EpsilonProfile(eps_i, eps_f)
+    #         fileName = "Q_{}_E{}_S{}_G{}_I{}_F{}".format("SXY", n_episodes, max_steps,
+    #                                                     gamma, eps_profile.initial,
+    #                                                     eps_profile.final)
+    #         controller = QAgent(game, eps_profile, gamma, alpha, fileName)
 
-            controller.loadQFromFile(
-                os.path.abspath(
-                    os.path.join("LearnedQ/",
-                                fileName + ".npy")))
+    #         controller.loadQFromFile(
+    #             os.path.abspath(
+    #                 os.path.join("LearnedQ/",
+    #                             fileName + ".npy")))
 
-            ### PLAY GAME ###
-            state = game.reset()
-            # startTime = time()
-            # while time() < startTime + 10:
-            while True:
-                action = controller.select_greedy_action(state)
-                state, reward, is_done = game.step(action)
-                # print(state)
-                # sleep(0.0001)
+    #         ### PLAY GAME ###
+    #         state = game.reset()
+    #         # startTime = time()
+    #         # while time() < startTime + 10:
+    #         while True:
+    #             action = controller.select_greedy_action(state)
+    #             state, reward, is_done = game.step(action)
+    #             # print(state)
+    #             # sleep(0.0001)
 
-            game.reset()
+    #         game.reset()
 
     # -------------------------------------------------------------------------
+    # TEST (ALL) PREVIOUSLY LEARNED AGENTS
 
     # gamma = 0.95
     # alpha = 1
@@ -74,6 +77,38 @@ def main():
     #     print(file, "    score: ", game.score_val)
 
     # -------------------------------------------------------------------------
+    # TEST EVOLUTION OF LEARNED AGENT
+
+    gamma = 0.95
+    alpha = 1
+    eps_profile = EpsilonProfile(0, 0)
+
+    directory = "QEvolution_Q_SXY_E200000_S500_G0.95_I0.6_F0.05"
+    logging.basicConfig(filename=directory+'.log', encoding='utf-8', level=logging.DEBUG)
+
+    file = 0
+    while file < 200000:
+        
+        controller = QAgent(game, eps_profile, gamma, alpha)
+        controller.loadQFromFile(os.path.join(os.path.abspath(directory), str(file) + ".npy"))
+
+        ### PLAY GAME ###
+        state = game.reset()
+        i = 0
+        while i < 20000:
+        # while True:
+            i += 1
+            action = controller.select_greedy_action(state)
+            state, reward, is_done = game.step(action)
+            # print(state)
+            # sleep(0.0001)
+
+        logging.debug("{:06d}".format(file) + ", " + str(game.score_val))
+        file += 1000
+
+    # -------------------------------------------------------------------------
+    # LEARN
+
     # for eps_i in [0.6]:
     #     for eps_f in [0.05]:
     #         for max_steps in [500]:
